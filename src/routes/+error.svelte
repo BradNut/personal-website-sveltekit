2ac689516e0c0dfbcbeebed3bd96ca330229ffe8
Bradley Shellnut <bradleyshellnut@pm.me>
2023-01-29 16:09:31 -0800

    Adding custom error page.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+</script>
+
+{#if $page.status === 404}
+	<h1>Sorry page not found! 🤷🏼</h1>
+	<p>You just hit a route that doesn't exist.</p>
+{/if}
+
+{#if $page.status === 500}
+	<h1>Sorry an unexpected error occurred! 😿</h1>
+	<p>Please try again later. </p>
+{/if}
