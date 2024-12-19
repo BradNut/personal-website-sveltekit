af47d0d0ba08926955ff1a5c473f591e9006b3a1
Bradley Shellnut <bradleyshellnut@pm.me>
2024-12-19 11:13:37 -0800

    Removing MDSvex and the markdown content.

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
-import mdsvexConfig from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
-	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
+	extensions: ['.svelte'],
+	preprocess: [vitePreprocess()],
 	vitePlugin: {
 		inspector: {
 			toggleKeyCombo: 'control-alt-shift',
